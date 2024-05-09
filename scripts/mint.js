9c47f3f0bb1fb2427d0a217df082821eb75e9baf
ashwath6105 <141135626+ashwath6105@users.noreply.github.com>
2024-05-09 18:38:23 +0530

    Update mint.js

--- scripts/mint.js
+++ scripts/mint.js
@@ -5,7 +5,7 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
-const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
+const tokenContractJSON = require("artifacts/contracts/Lambo.sol/Lamborghini.json");
 require('dotenv').config()
 
 const tokenAddress = ""; // place your erc20 contract address here
